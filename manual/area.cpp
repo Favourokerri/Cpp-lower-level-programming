462f524bda785f1d4025d8b94b4eed360e16187f
Favourokerri <favourokerri767@gmail.com>
2023-11-11 19:00:25 +0000

    area

--- manual/area.cpp
+++ manual/area.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+/**
+ * a program that takes raduis from the user and calculate 
+ * the area of a sphere
+ */
+using namespace std;
 
 int main()
 {
-	double radius = 0;
-	double Area = 4*p*r
+	double r = 0;
+	double p = 3.1416;
+	double Area = 0;
+	cout <<"please enter radius:";
+	cin >> r;
+	Area = 4 * p * (r * r);
+	cout << " the area for radius "<< r <<" is \a"<< Area << endl;
+
+	return (0);
+}
